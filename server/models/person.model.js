c7a73d23046d2aacb6de90f79125f9acf77723cd
ben <ben@surgison.net>
2019-07-12 12:58:03 +0100

    Connect the api and front office

--- server/models/person.model.js
+++ server/models/person.model.js
@@ -10,8 +10,7 @@ module.exports = class Person extends BaseModel {
 
   static get schema () {
     return {
-      firstName: Joi.string().example('Jon'),
-      lastName: Joi.string().example('Doe'),
+      fullName: Joi.string().example('Jon Doe'),
       email: Joi.string().email().example('jon.doe@test.defra.net'),
       addressId: Joi.string().guid().example('f73a9ea5-dc20-494a-abb7-89ef60c99715')
     }
