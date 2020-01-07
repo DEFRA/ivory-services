30c25a2f0ba6caca0efdf1b37c11ae1c4e9e9e92
ben <ben@surgison.net>
2020-01-07 16:55:29 +0000

    Fix random id generator

--- server/utils/registration-number-generator.js
+++ server/utils/registration-number-generator.js
@@ -20,7 +20,24 @@ module.exports = {
   },
 
   includesAtLeastOneOf (id, set) {
-    return id.some((char) => set.includes(char))
+    return id.split('').some((char) => set.includes(char))
+  },
+
+  // Make sure no sequence of characters contain profanity
+  isClean: function (id) {
+    for (let pos = 0; pos < id.length; pos++) {
+      for (let len = 0; len < id.length; len++) {
+        const result = filter.clean(id.substr(pos, len))
+        if (result.includes('*')) {
+          return false
+        }
+      }
+    }
+    return true
+  },
+
+  isAllowed: function (id) {
+    return this.includesAtLeastOneOf(id, this.characters) && this.includesAtLeastOneOf(id, this.numbers) && this.isClean(id)
   },
 
   get randomCharacter () {
@@ -30,24 +47,16 @@ module.exports = {
 
   random: async function () {
     let id
+    let passes
     // Build array of 8 random alphanumerics
-    do { for (id = []; id.length < 8; id.push(this.randomCharacter)) {} }
-    // Make sure that there is at least one number and at least one character
-    while (!this.includesAtLeastOneOf(id, this.characters) && !this.includesAtLeastOneOf(id, this.numbers) && !this.bad(id.join('')))
-    // Return the 8 character string
-    return id.join('')
-  },
-
-  // Make sure no sequence of characters contain profanity
-  bad: function (id) {
-    for (let pos = 0; pos < id.length; pos++) {
-      for (let len = 0; len < id.length; len++) {
-        const result = filter.clean(id.substr(pos, len))
-        if (result.includes('*')) {
-          return true
-        }
-      }
+    do {
+      for (id = []; id.length < 8; id.push(this.randomCharacter)) {}
+      id = id.join('')
+      passes = this.isAllowed(id)
     }
-    return false
+    // Make sure that there is at least one number and at least one character
+    while (!passes)
+    // Return the 8 character string
+    return id
   }
 }
--- server/utils/registration-number-generator.test.js
+++ server/utils/registration-number-generator.test.js
@@ -7,27 +7,39 @@ const registrationNumber = require('./registration-number-generator')
 lab.experiment(TestHelper.getFile(__filename), () => {
   lab.experiment('registrationNumber', () => {
     lab.test('bad should be true', async () => {
-      Code.expect(await registrationNumber.bad('1ARSEMX3')).to.equal(true)
+      Code.expect(await registrationNumber.isClean('1ARSEMX3')).to.equal(false)
     })
 
     lab.test('bad should be false', async () => {
-      Code.expect(await registrationNumber.bad('1FTREEX3')).to.equal(false)
+      Code.expect(await registrationNumber.isClean('1FTREEX3')).to.equal(true)
     })
 
     lab.test('includesAtLeastOneOf passes for charaters', async () => {
-      const id = '12345A78'.split('')
-      Code.expect(await registrationNumber.includesAtLeastOneOf(id, 'ACDEFGHJ'.split(''))).to.equal(true)
+      Code.expect(await registrationNumber.includesAtLeastOneOf('12345A78', 'ACDEFGHJ'.split(''))).to.equal(true)
     })
 
     lab.test('includesAtLeastOneOf passes for numbers', async () => {
-      const id = '12345A78'.split('')
-      Code.expect(await registrationNumber.includesAtLeastOneOf(id, '234679'.split(''))).to.equal(true)
+      Code.expect(await registrationNumber.includesAtLeastOneOf('12345A78', '234679'.split(''))).to.equal(true)
     })
 
-    lab.test('get', async () => {
-      const id = await registrationNumber.random()
-      console.log(id)
-      Code.expect(id.length).to.equal(8)
+    lab.test('isAllowed passes when correct', async () => {
+      Code.expect(await registrationNumber.isAllowed('12345A78')).to.equal(true)
+    })
+
+    lab.test('isAllowed fails when not allowed', async () => {
+      Code.expect(await registrationNumber.isAllowed('1ARSEMX3')).to.equal(false)
+      Code.expect(await registrationNumber.isAllowed('12345678')).to.equal(false)
+      Code.expect(await registrationNumber.isAllowed('ABCDEFGHI')).to.equal(false)
+      Code.expect(await registrationNumber.isAllowed('1FTREEX3')).to.equal(true)
+    })
+
+    lab.test('random', async () => {
+      for (let i = 0; i < 20; i++) {
+        const id = await registrationNumber.random()
+        Code.expect(await registrationNumber.isClean(id)).to.equal(true)
+        Code.expect(await registrationNumber.isAllowed(id)).to.equal(true)
+        Code.expect(id.length).to.equal(8)
+      }
     })
   })
 })
